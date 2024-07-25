dc1a54ef8d2ff90767cc78c6cd5800933c4bfd19
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-25 19:00:39 +0900

    Feat: 테스트 실행 파일 지정 cli 개발

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
-import readline from "readline";
+import fs from "fs";
+import path from "path";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+import readline from "node:readline/promises";
+import { stdin, stdout } from "node:process";
 
-rl.on();
+import { exec } from "child_process";
+import { match, P } from "ts-pattern";
+
+const readDirs = () => {
+  const files = fs.readdirSync(path.resolve(__dirname, "."));
+  const exceptRootFile = files.filter((file) => file !== "index.ts");
+  return exceptRootFile.map((file, idx) => `${idx + 1}: ${file}`);
+};
+
+const spawnProcess = (file: string) => {
+  exec(`tsx ./src/${file}`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`error: ${err}`);
+      return;
+    }
+
+    console.log(stdout);
+  });
+};
+
+(async () => {
+  const rl = readline.createInterface({ input: stdin, output: stdout });
+  const choiceArray = readDirs();
+  const choiceString = choiceArray.join(`\n`);
+  const questionMessage = "테스트할 메소드의 번호를 선택해 주세요.";
+  const answer = await rl.question(`${questionMessage}\n${choiceString}\n`);
+
+  match(Number(answer))
+    .with(NaN, () => console.log("NaN입니다."))
+    .with(P.number, () => {
+      const splitFileName = choiceArray[Number(answer) - 1].split(" ")[1];
+      spawnProcess(splitFileName);
+    });
+})();
