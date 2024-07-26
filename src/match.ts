5b404e926633da69f9278731b30f0914546c62fa
Jiwoo So <58384366+jiwooproity@users.noreply.github.com>
2024-07-26 15:55:16 +0900

    Fix: match run method in function

--- src/match.ts
+++ src/match.ts
@@ -2,13 +2,11 @@ import { match, P } from "ts-pattern";
 
 const valueArray = [1, "string", true];
 
-const matchMethod = () => {
+(() => {
   valueArray.forEach((value) => {
     match(value)
       .with(P.number, () => console.log(`${value}는 숫자 타입입니다.`))
       .with(P.string, () => console.log(`${value}는 문자열 타입입니다.`))
       .with(P.boolean, () => console.log(`${value}는 불리언 타입입니다.`));
   });
-};
-
-matchMethod();
+})();
